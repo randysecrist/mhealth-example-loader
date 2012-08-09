371caf48c903c6955c85c0fd3b10201399d6173e
Bryce Kerley <bkerley@brycekerley.net>
2012-08-09 12:26:13 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MhealthExampleLoader::Application.config.secret_token = 'fd1ebfa81e59a9a5cda4975e7534864c2e004aaa165bc06555152b1cb056e1431dc3666803ccdc54fdf96436ff6847ebcf1a6073cbee3d3464ddc15f6f394e96'
