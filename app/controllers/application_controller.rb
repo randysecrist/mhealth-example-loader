0557a899cae300726985a069bbbc1c3f4769d97e
Bryce Kerley <bkerley@brycekerley.net>
2012-08-10 19:42:22 -0400

    client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,18 @@ class ApplicationController < ActionController::Base
   def authorized?
     !(session[:mhealth_token].blank?)
   end
+
+  def api_client
+    return @api_client if defined? @api_client
+    raise "No token" unless authorized?
+
+    @api_client = self.class.api_client_for(session[:mhealth_token])
+  end
+
+  def self.api_client_for(token)
+    Faraday.new 'https://api-mhealth.att.com/' do |builder|
+      builder.use Faraday::Request::Authorization, 'OAuth', token
+      builder.adapter :net_http
+    end
+  end
 end
