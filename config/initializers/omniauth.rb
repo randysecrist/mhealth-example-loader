c61e58ec5616c48d0bd5f0c08ba8788d78477479
Bryce Kerley <bkerley@brycekerley.net>
2012-09-20 10:28:26 -0400

    use next for mhealth oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider(:mhealth, ENV['MHEALTH_KEY'], ENV['MHEALTH_SECRET'], setup:(lambda do |env|
+  provider(:mhealth, ENV['MHEALTH_KEY'], ENV['MHEALTH_SECRET'], 
+           client_options: {
+            site: 'https://mhealth.next.attcompute.com/'
+            },
+           setup:(lambda do |env|
     request = Rack::Request.new env
     scopes = %w{/read/health/user}
     if request.params['app']
